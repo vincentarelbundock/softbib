a270a9e53ca1becaa5d06940f7e5842b2d4fb6e9
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-12-06 12:50:36 -0500

    output and output_dir

--- R/softbib.R
+++ R/softbib.R
@@ -1,48 +1,3 @@
-get_keys <- function(path) {
-    names(bibtex::read.bib(path))
-}
-
-write_bib <- function(pkgnames, path) {
-    bibtex::write.bib(pkgnames, path)
-    # make sure everything is UTF-8
-    # https://stackoverflow.com/questions/7481799/convert-a-file-encoding-using-r-ansi-to-utf-8
-    writeLines(
-        iconv(readLines(path), from = "", to = "UTF8"), 
-        file(path, encoding="UTF-8"))
-}
-
-get_dependencies <- function(path) {
-    sort(unique(renv::dependencies(path = path)$Package))
-}
-
-write_rmarkdown <- function(fn_bibtex,
-                            fn_rmarkdown,
-                            style,
-                            keys) {
-    rmd_template <- c(
-        "---",
-        "bibliography: %s",
-        "nocite: |",
-        "  %s",
-        "csl: %s",
-        "---",
-        "",
-        "# Software Bibliography",
-        "")
-    keys <- paste(paste0("@", keys), collapse = ", ")
-    if (is.null(style)) {
-        rmd_template <- rmd_template[!grepl("^csl", rmd_template)]
-        rmd_template <- paste(rmd_template, collapse = "\n")
-        out <- sprintf(rmd_template, fn_bibtex, keys)
-    } else {
-        rmd_template <- paste(rmd_template, collapse = "\n")
-        out <- sprintf(rmd_template, fn_bibtex, keys, style)
-    }
-    writeLines(
-        out,
-        file(fn_rmarkdown, encoding = "UTF-8"))
-}
-
 
 #' Software Bibliographies
 #'
@@ -50,6 +5,8 @@ write_rmarkdown <- function(fn_bibtex,
 #' automatically creates software bibliographies in PDF, Word, Rmarkdown and
 #' BibTeX formats.
 #'
+#' @param output vector of file paths or `NULL`. Acceptable file name extensions: .bib, .Rmd, .docx, .pdf. `NULL` returns a vector of BibTeX cite keys.
+#' @param output_dir path of the directory where files should be saved.
 #' @param include `NULL` or character vector.
 #' + `NULL`: the working directory is crawled to identify `R` packages used in the project, and all packages are included in the software bibliography.
 #' + Character vector: the names of packages to include in the bibliography.
@@ -81,6 +38,8 @@ write_rmarkdown <- function(fn_bibtex,
 #' }
 #' @export
 softbib <- function(
+    output = c("softbib.pdf", "softbib.docx", "softbib.bib", "softbib.Rmd"),
+    output_dir = getwd(),
     include = NULL,
     exclude = NULL,
     style = NULL) {
@@ -92,19 +51,57 @@ softbib <- function(
     # sanity checks
     checkmate::assert_character(exclude, null.ok = TRUE)
     checkmate::assert_character(include, null.ok = TRUE)
+    checkmate::assert_directory_exists(output_dir)
     if (!is.null(style)) {
         checkmate::assert_file_exists(style, extension = "csl")
         style <- path.expand(style)
     }
-
     if (!is.null(include) && !is.null(exclude)) {
         stop("The `include` and `exclude` arguments cannot be used simultaneously.", call. = FALSE)
     }
+    if (!is.null(output)) {
+        extensions <- tools::file_ext(output)
+        flag <- checkmate::check_true(all(extensions %in% c("pdf", "docx", "bib", "Rmd")))
+        if (!isTRUE(flag)) {
+            stop('The `output` argument must be a vector of file paths with the following extensions: ".pdf", ".docx", ".bib", ".Rdm"', call. = FALSE)
+        }
+        if (anyDuplicated(extensions)) {
+            stop("The `output` argument cannot include multiple paths with the same extension.", call. = FALSE)
+        }
+    }
 
-    fn_rmarkdown <- file.path(project, paste0(bibliography_name, ".Rmd"))
-    fn_pdf <- file.path(project, paste0(bibliography_name, ".pdf"))
-    fn_word <- file.path(project, paste0(bibliography_name, ".docx"))
-    fn_bibtex <- file.path(project, paste0(bibliography_name, ".bib"))
+    # paths and unlink flags
+    get_unlink <- function(ext) {
+        regex <- paste0("\\.", ext, "$")
+        out <- !any(grepl(regex, output))
+        return(out)
+    }
+
+    get_path <- function(ext) {
+        regex <- paste0("\\.", ext, "$")
+        out <- output[grepl(regex, output)]
+        if (!isTRUE(checkmate::check_path_for_output(out, overwrite = TRUE))) {
+            out <- paste0(tempfile(), ".", ext)
+        }
+        return(out)
+    }
+
+    unlink_pdf <- get_unlink("pdf")
+    unlink_bib <- get_unlink("bib")
+    unlink_Rmd <- get_unlink("Rmd")
+    unlink_docx <- get_unlink("docx")
+
+
+    fn_Rmd <- get_path("Rmd")
+    fn_bib <- get_path("bib")
+    fn_pdf <- get_path("pdf")
+    fn_docx <- get_path("docx")
+
+    # without bibtex, markdown file is useless
+    if (isFALSE(unlink_Rmd) && isTRUE(unlink_bib)) {
+        unlink_bib <- FALSE
+        fn_bib <- gsub("Rmd$", "bib", fn_Rmd)
+    }
 
     if (is.null(include)) {
         deps <- get_dependencies(project)
@@ -132,17 +129,87 @@ softbib <- function(
         stop("Could not find an included `R` package.", call. = FALSE)
     }
 
-    write_bib(deps, fn_bibtex)
+    if (!isTRUE(checkmate::check_path_for_output(fn_bib))) {
+        fn_bib <- paste0(tempfile(), ".bib")
+    }
 
-    keys <- get_keys(fn_bibtex)
+    write_bib(deps, fn_bib)
+
+    keys <- get_keys(fn_bib)
+
+    if (is.null(output)) {
+        unlink(fn_bib)
+        return(invisible(keys))
+    }
+
+    if (isTRUE(unlink_Rmd) && isTRUE(unlink_pdf) && isTRUE(unlink_docx)) {
+        return(invisible(keys))
+    }
 
     write_rmarkdown(
-        fn_bibtex,
-        fn_rmarkdown,
+        fn_bib,
+        fn_Rmd,
         style = style,
         keys = keys)
 
-    rmarkdown::render(input = fn_rmarkdown, output_file = fn_pdf)
-    rmarkdown::render(input = fn_rmarkdown, output_file = fn_word)
+    if (!isTRUE(unlink_pdf)) {
+        rmarkdown::render(input = fn_Rmd, output_file = fn_pdf, output_dir = output_dir)
+    }
+    if (!isTRUE(unlink_docx)) {
+        rmarkdown::render(input = fn_Rmd, output_file = fn_docx, output_dir = output_dir)
+    }
+    
+    if (isTRUE(unlink_bib)) unlink(fn_bib)
+    if (isTRUE(unlink_Rmd)) unlink(fn_Rmd)
+
+    return(invisible(keys))
 }
 
+
+get_keys <- function(path) {
+    names(bibtex::read.bib(path))
+}
+
+
+write_bib <- function(pkgnames, path) {
+    bibtex::write.bib(pkgnames, path)
+    # make sure everything is UTF-8
+    # https://stackoverflow.com/questions/7481799/convert-a-file-encoding-using-r-ansi-to-utf-8
+    writeLines(
+        iconv(readLines(path), from = "", to = "UTF8"), 
+        file(path, encoding="UTF-8"))
+}
+
+
+get_dependencies <- function(path) {
+    sort(unique(renv::dependencies(path = path)$Package))
+}
+
+
+write_rmarkdown <- function(fn_bib,
+                            fn_Rmd,
+                            style,
+                            keys) {
+    rmd_template <- c(
+        "---",
+        "bibliography: %s",
+        "nocite: |",
+        "  %s",
+        "csl: %s",
+        "---",
+        "",
+        "# Software Bibliography",
+        "")
+    keys <- paste(paste0("@", keys), collapse = ", ")
+    if (is.null(style)) {
+        rmd_template <- rmd_template[!grepl("^csl", rmd_template)]
+        rmd_template <- paste(rmd_template, collapse = "\n")
+        out <- sprintf(rmd_template, fn_bib, keys)
+    } else {
+        rmd_template <- paste(rmd_template, collapse = "\n")
+        out <- sprintf(rmd_template, fn_bib, keys, style)
+    }
+    writeLines(
+        out,
+        file(fn_Rmd, encoding = "UTF-8"))
+}
